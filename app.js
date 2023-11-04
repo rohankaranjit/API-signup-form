2f5a8ebae86a2ce0cba8fbb54c6c28f09eb209ae
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-11-04 23:39:14 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+
+
+    app.post("/",function(req,res){
+
+        var firstName = req.body.fName;
+
+        var lastName = req.body.lname;
+        var email = req.body.email;
+
+        console.log(firstName, lastName, email);
+
+    })
+    
+
+});
+app.listen(3000,function(){
+    console.log("Server is running in 3000.");
+})
