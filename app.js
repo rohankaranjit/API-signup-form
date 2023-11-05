59a7e83a22bed388c6cd3f620739fa7b542acb52
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-11-05 21:51:33 +0545

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const https = require("https")
 const app = express();
 
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({extended : true}));
-
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/signup.html");
 });
