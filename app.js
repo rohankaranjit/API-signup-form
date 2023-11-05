e7a0fff9bfe9761c208ab35f8a2b9eaf86b6d63a
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-11-05 21:50:59 +0545

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.get("/",function(req,res){
     app.post("/",function(req,res){
 
         const firstName = req.body.fName;
-
         const lastName = req.body.lname;
         const email = req.body.email;
 
